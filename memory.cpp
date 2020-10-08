687634ca373bf7481148372a719d0d20a98d138c
Negin Firouzian <negfir@gmail.com>
2020-10-08 22:16:12 +0330

    mem done

--- memory.cpp
+++ memory.cpp
@@ -1,41 +1,52 @@
 #include "systemc.h"
 #include "memory_if.h"
+#include <fstream>
 #define MEM_SIZE 500
 // this class implements the virtual functions
 // in the interfaces
 class memory: public sc_module, public simple_mem_if
 {
   private:
-    char data[20];
+    unsigned int memData[MEM_SIZE]={};
 
   public:
   // constructor
-    memory(sc_module_name nm) : sc_module(nm)
+    memory(sc_module_name nm, char* file) : sc_module(nm)
     {
+      ifstream initFile("mem.txt");
+      int cnt= 0;
+      int x;
+       while (initFile >> x){
+        // arr[cnt++] = x;
+        this->Write(cnt++, x);
+      }
+
     }
-    bool memory::nb_write(char c)
+    bool memory::Write(unsigned int addr, unsigned int data)
     {
-      if (top < 20)
+      if (addr < MEM_SIZE)
       {
-        data[top++] = c;
+        this->memData[addr]=data;
         return true;
       }
       return false;
       }
 
-    bool memory::nb_read(char& c)
+    bool memory::Read(unsigned int addr, unsigned int& data)
     {
-    if (top > 0)
-    {
-      c = data[--top];
-      return true;
-    }
-    return false;
-    }
-    void memory::register_port(sc_port_base& port_, const char* if_typename_)
-    {
-      cout << "binding " << port_.name() << " to " << "interface: " << if_typename_ << endl;
-    }
+      if (addr < MEM_SIZE)
+      {
+        data=this->memData[addr];
+        return true;
+      }
+      return false;
+      }
+
+
+    // void memory::register_port(sc_port_base& port_, const char* if_typename_)
+    // {
+    //   cout << "binding " << port_.name() << " to " << "interface: " << if_typename_ << endl;
+    // }
   };
 
 
